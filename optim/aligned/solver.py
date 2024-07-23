bcc24475c79dabee418afa7d2bf5e68a2b0de12b
rkhosrowshahli <rasa.khosrowshahli@ontariotechu.net>
2024-07-23 07:36:04 -0400

    eig is not helpful here, eigh it is

--- optim/aligned/solver.py
+++ optim/aligned/solver.py
@@ -13,7 +13,7 @@ class ProcrustesSolver:
             cov_grad_matrix_e = cov_grad_matrix_e.mean(0)
 
             # singulars, basis = torch.symeig(cov_grad_matrix_e, eigenvectors=True) ***DEPRECATED***
-            singulars, basis = torch.linalg.eig(
+            singulars, basis = torch.linalg.eigh(
                 cov_grad_matrix_e
             )  # singular is eigenvalue landa and basis is eigenvector V
             print(

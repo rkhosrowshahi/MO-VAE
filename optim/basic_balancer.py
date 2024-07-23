92a245549f29f9c893df111d2505d19fb2d13771
rkhosrowshahli <rasa.khosrowshahli@ontariotechu.net>
2024-07-23 10:29:54 -0400

    fix bug

--- optim/basic_balancer.py
+++ optim/basic_balancer.py
@@ -234,7 +234,7 @@ class BasicBalancer(torch.nn.Module):
             shared_params=list(model.encoder.parameters()),
             task_specific_params={
                 "reconstruction": model.decoder,
-                "kl": torch.nn.Sequential([model.mu, model.log_var]),
+                "kl": torch.nn.Sequential(model.mu, model.log_var),
             },
             shared_representation=hrepr,
             last_shared_layer_params=None,

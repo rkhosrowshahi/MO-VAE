40a0dc87e77a93cd8e4b107915adaa4461e14fae
rkhosrowshahli <rasa.khosrowshahli@ontariotechu.net>
2025-11-21 12:51:32 -0500

    Remove deterministic settings for cuDNN in set_seed function to improve compatibility and performance across different hardware setups.

--- utils/utils.py
+++ utils/utils.py
@@ -6,8 +6,6 @@ def set_seed(seed):
     np.random.seed(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
-    torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = False
 
 class AverageMeter:
     def __init__(self):
